5c5fffc90d9ac4545e906acb787de00aabf39a8f
Wei Wang <weiwio@outlook.com>
2020-06-27 06:19:40 +0000

    Custom cache

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 const withPWA = require('next-pwa');
+const runtimeCaching = require('./cache')
 
 module.exports = withPWA({
   pwa: {
     dest: 'public',
     precacheHomePage: false,
     additionalManifestEntries: [],
+    runtimeCaching
   },
   pageExtensions: ['page.js'],
 });
